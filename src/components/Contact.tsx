6f15ec390c267af852f201480bbec75e3d71cfd8
Devon Wieczorek <devon.wieczorek93@gmail.com>
2024-08-02 16:03:57 -0400

    fix(Contact): adds github link to contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,14 +4,26 @@ import s from "../styles/Contact.less";
 const Contact: React.FC = () => {
   return (
     <div className={s.contact}>
-      to contact devon, email him at&nbsp;
-      <a 
-        href="mailto:devon.wieczorek@icloud.com?subject=I%20Love%20Your%20Website!" 
-        target="_blank" 
-        rel="noreferrer"
-      >
-        devon.wieczorek@icloud.com
-      </a>
+      <div>
+        To contact devon, email him at&nbsp;
+        <a 
+          href="mailto:devon.wieczorek@icloud.com?subject=I%20Love%20Your%20Website!" 
+          target="_blank" 
+          rel="noreferrer"
+        >
+          devon.wieczorek@icloud.com
+        </a>.
+      </div>
+      <div>
+        You can also view some of Devon's projects on his&nbsp;
+        <a 
+          href="https://github.com/DevonWieczorek"
+          target="_blank" 
+          rel="noreferrer"
+          >
+            Github
+          </a>
+      </div>
     </div>
   );
 };
