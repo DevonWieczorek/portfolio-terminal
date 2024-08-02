7e04de53a59e5c46795138e25ef2e6cf8984cce7
Devon Wieczorek <devon.wieczorek93@gmail.com>
2024-08-02 15:37:28 -0400

    feat(HelpMenu): adds HelpMenu component

---
+++ src/components/HelpMenu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HelpMenu: React.FC = () => (
+    <>
+      <p>commands:</p>
+      <p>resume - todo: add resume</p>
+      <p>contact - find out where to reach Devon</p>
+      <p>ask - asks AI Devon a question</p>
+      <p>clear - clear the console</p>
+      <p>help - displays this menu</p>
+    </>
+  );
+
+  export default HelpMenu;
