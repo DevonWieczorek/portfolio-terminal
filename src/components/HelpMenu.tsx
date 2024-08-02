187844f7c9d96ae2e6b207cbac20c47dcd0efbae
Devon Wieczorek <devon.wieczorek93@gmail.com>
2024-08-02 16:10:06 -0400

    fix(HelpMenu): adds grid styling

--- src/components/HelpMenu.tsx
+++ src/components/HelpMenu.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-
-const HelpMenu: React.FC = () => (
-    <>
+const HelpMenu = () => (
+    <div style={{display: 'grid', gridGap: 'var(--grid-gap)'}}>
       <p>commands:</p>
       <p>resume - todo: add resume</p>
       <p>contact - find out where to reach Devon</p>
       <p>ask - asks AI Devon a question</p>
       <p>clear - clear the console</p>
       <p>help - displays this menu</p>
-    </>
+    </div>
   );
 
   export default HelpMenu;
