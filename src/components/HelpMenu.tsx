36837f0995c333a86cfae0390f4e0569eeb00970
Devon Wieczorek <devon.wieczorek93@gmail.com>
2024-09-11 15:35:11 -0400

    fix(HelpMenu): updates options

--- src/components/HelpMenu.tsx
+++ src/components/HelpMenu.tsx
@@ -1,11 +1,12 @@
 const HelpMenu = () => (
     <div style={{display: 'grid', gridGap: 'var(--grid-gap)'}}>
       <p>commands:</p>
-      <p>resume - todo: add resume</p>
-      <p>contact - find out where to reach Devon</p>
-      <p>ask - asks AI Devon a question</p>
-      <p>clear - clear the console</p>
-      <p>help - displays this menu</p>
+      <p>resume: displays plain-text version of Devon's resume</p>
+      <p>contact: find out where to reach Devon</p>
+      <p>ask: asks AI Devon a question</p>
+      <p>fun-fact: displays a random "fun fact" about Devon</p>
+      <p>clear: clear the console</p>
+      <p>help: displays this menu</p>
     </div>
   );
 
