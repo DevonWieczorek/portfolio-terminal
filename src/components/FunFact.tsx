0a5ff5560612a40686733563abbf49994b4b1ffd
Devon Wieczorek <devon.wieczorek93@gmail.com>
2025-06-13 15:34:15 -0400

    chore: adds TODOs

--- src/components/FunFact.tsx
+++ src/components/FunFact.tsx
@@ -1,22 +1,26 @@
+import React from "react";
 
-import React, { useState } from "react";
+// TODO: get fun facts from GPT or have them both pull from a single source
 
 const funFacts = [
-    "In addition to his interest in coding, Devon is also a huge film buff! He attended the New York Film Academy for producing in 2012.",
+	"In addition to his interest in coding, Devon is also a huge film buff! He attended the New York Film Academy for producing in 2012.",
 	"Devon once owned and ran his own skateboarding company, Brew Crew Skateboards, with his brother and cousin.",
 	"Devon is also a musician! He plays the bass and is a vocalist in the punk rock band, Friend Z.",
 	"Devon loves to travel! He enjoys seeing all the world has to offer and wants to see every country he possibly can.",
-  ];
+];
 
 const FunFact: React.FC = () => {
-  const randomIndex = Math.floor(Math.random() * funFacts.length);
+	const randomIndex = Math.floor(Math.random() * funFacts.length);
 
-  return (
-    <div>
-		<div>Devon Fun Fact:</div>
-		<div>{funFacts[randomIndex]}</div>
-	</div>
-  );
+	// TODO: remove fun fact from array after it's displayed
+	// After all fun facts are displayed, display his contact info and prompt to ask a question
+
+	return (
+		<div>
+			<div>Devon Fun Fact:</div>
+			<div>{funFacts[randomIndex]}</div>
+		</div>
+	);
 };
 
 export default FunFact;
