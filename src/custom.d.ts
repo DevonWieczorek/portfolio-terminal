2b125b493524e3b559760edff50416d92a45d609
Devon Wieczorek <devon.wieczorek93@gmail.com>
2025-07-11 17:40:42 -0400

    fix: formatting

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,9 +2,9 @@ declare module "*.pdf" {
 	const content: string;
 	export default content;
 }
-declare module 'pdf-parse';
-declare module '*.less';
+declare module "pdf-parse";
+declare module "*.less";
 declare module "*.md" {
 	const content: string;
 	export default content;
-  }
+}
