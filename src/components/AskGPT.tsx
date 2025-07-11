3fd1699956cb763bce59798224557694c2cb2da3
Devon Wieczorek <devon.wieczorek93@gmail.com>
2025-07-11 17:37:36 -0400

    fix(AskGPT): updates types and fixes formatting

--- src/components/AskGPT.tsx
+++ src/components/AskGPT.tsx
@@ -6,101 +6,101 @@ import { useOpenAIAssistant } from "../hooks/useGPT";
 import s from "../styles/AskGPT.less";
 
 type FormattedResponseProps = {
-  response: string;
-  query?: string;
-  onShowResponse?: Function;
+	response: string;
+	query?: string;
+	onShowResponse?: () => void;
 }
 
 const FormattedResponse: React.FC<FormattedResponseProps> = ({ response, query, onShowResponse }) => {
-  useEffect(() => {
-    if (onShowResponse && response) {
-      onShowResponse();
-    }
-  }, [response, onShowResponse]);
+	useEffect(() => {
+		if (onShowResponse && response) {
+			onShowResponse();
+		}
+	}, [response, onShowResponse]);
 
-  if (response){
-    return (
-      <div className={s.askGpt}>
-        {query && <div>{query}</div>}
-        <div>
-          <Markdown>{response}</Markdown>
-        </div>
-      </div>
-    );
-  }
-  return null;
+	if (response) {
+		return (
+			<div className={s.askGpt}>
+				{query && <div>{query}</div>}
+				<div>
+					<Markdown>{response}</Markdown>
+				</div>
+			</div>
+		);
+	}
+	return null;
 }
 
-const AskGPT: React.FC <{ onShowResponse: Function }>= ({ onShowResponse }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [query, setQuery] = useState<string>("");
-  const [inputValue, setInputValue] = useState<string>("");
-  const [displayQuery, setDisplayQuery] = useState<string>("");
-  const [response, setResponse] = useState<string>("");
-  const [, queryOpenAIAssistant] = useOpenAIAssistant({
-    onSuccess: (response: string) => setFormattedResponse(response),
-    onError: (error: string) => setFormattedResponse(error),
-  });
+const AskGPT: React.FC<{ onShowResponse: () => void }> = ({ onShowResponse }) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+	const [query, setQuery] = useState<string>("");
+	const [inputValue, setInputValue] = useState<string>("");
+	const [displayQuery, setDisplayQuery] = useState<string>("");
+	const [response, setResponse] = useState<string>("");
+	const [, queryOpenAIAssistant] = useOpenAIAssistant({
+		onSuccess: (response: string) => setFormattedResponse(response),
+		onError: (error: string) => setFormattedResponse(error),
+	});
 
-  const setFormattedResponse = (response: string) => {
-    if(response) {
-      const formattedResponse = removeCitations(response);
-      setResponse(formattedResponse);
-    }
-  };
+	const setFormattedResponse = (response: string) => {
+		if (response) {
+			const formattedResponse = removeCitations(response);
+			setResponse(formattedResponse);
+		}
+	};
 
-  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-    setQuery(event.target.value);
-  };
+	const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setInputValue(event.target.value);
+		setQuery(event.target.value);
+	};
 
-  const handleSubmit = async () => {
-    if (!query) return;
+	const handleSubmit = async () => {
+		if (!query) return;
 
-    if (inputRef?.current) {
-      setInputValue("asking devon-gpt, this may take a moment... ");
-      setDisplayQuery(query);
-      inputRef.current.blur();
-    }
+		if (inputRef?.current) {
+			setInputValue("asking devon-gpt, this may take a moment... ");
+			setDisplayQuery(query);
+			inputRef.current.blur();
+		}
 
-    queryOpenAIAssistant(query);
-  };
+		queryOpenAIAssistant(query);
+	};
 
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>): void => {
-    if (isEnterKeyPress(event)) {
-      handleSubmit();
-    }
-  };
+	const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+		if (isEnterKeyPress(event)) {
+			handleSubmit();
+		}
+	};
 
-  useEffect(() => {
-    if (inputRef?.current) {
-      inputRef.current.focus();
-    }
-  });
+	useEffect(() => {
+		if (inputRef?.current) {
+			inputRef.current.focus();
+		}
+	}, []);
 
-  if (response){
-    return <FormattedResponse response={response} query={query} onShowResponse={onShowResponse} />;
-  }
- 
-  return (
-    <div className={s.askGpt}>
-      {displayQuery ? <div>{displayQuery}</div> : <div>press ENTER to submit:</div>}
-      <div className={s.inputArea}>
-        <input
-          ref={inputRef}
-          type="text"
-          value={inputValue}
-          onKeyPress={handleKeyPress}
-          onChange={handleQueryChange}
-          placeholder="ask a question to devon-gpt..."
-          className={s.askGptInput}
-        />
-        <button onClick={handleSubmit} className={s.askGptButton}>
-          submit
-        </button>
-      </div>
-    </div>
-  );
+	if (response) {
+		return <FormattedResponse response={response} query={query} onShowResponse={onShowResponse} />;
+	}
+
+	return (
+		<div className={s.askGpt}>
+			{displayQuery ? <div>{displayQuery}</div> : <div>press ENTER to submit:</div>}
+			<div className={s.inputArea}>
+				<input
+					ref={inputRef}
+					type="text"
+					value={inputValue}
+					onKeyPress={handleKeyPress}
+					onChange={handleQueryChange}
+					placeholder="ask a question to devon-gpt..."
+					className={s.askGptInput}
+				/>
+				<button onClick={handleSubmit} className={s.askGptButton}>
+					submit
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default AskGPT;
