b2f9403c0bbe4c4bf32776fdf21df3e90c535809
Devon Wieczorek <devon.wieczorek93@gmail.com>
2025-08-14 17:41:39 -0400

    fix(Room): moves wallColor to SceneContext

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -4,11 +4,11 @@ import { useScene } from "../lib/contexts/SceneContext";
 import Desk from "./Desk";
 
 export default function Room() {
-  const { roomSize, wallHeight, wallThickness } = useScene();
-  
+  const { roomSize, wallColor, wallHeight, wallThickness } = useScene();
+
   // Load wood texture for floor
   const floorTexture = useTexture("/textures/wood.jpg");
-  
+
   // Configure texture repeat for wooden floor
   floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
   floorTexture.repeat.set(8, 8); // Wood plank pattern repeat
@@ -26,27 +26,27 @@ export default function Room() {
       {/* North Wall */}
       <mesh position={[0, wallHeight / 2, -roomSize / 2]} receiveShadow>
         <boxGeometry args={[roomSize, wallHeight, wallThickness]} />
-        <meshLambertMaterial color="#F5F5DC" />
+        <meshLambertMaterial color={wallColor} />
       </mesh>
 
       {/* South Wall */}
       <mesh position={[0, wallHeight / 2, roomSize / 2]} receiveShadow>
         <boxGeometry args={[roomSize, wallHeight, wallThickness]} />
-        <meshLambertMaterial color="#F5F5DC" />
+        <meshLambertMaterial color={wallColor} />
       </mesh>
 
       {/* East Wall */}
       <mesh position={[roomSize / 2, wallHeight / 2, 0]} receiveShadow>
         <boxGeometry args={[wallThickness, wallHeight, roomSize]} />
-        <meshLambertMaterial color="#F5F5DC" />
+        <meshLambertMaterial color={wallColor} />
       </mesh>
 
       {/* West Wall */}
       <mesh position={[-roomSize / 2, wallHeight / 2, 0]} receiveShadow>
         <boxGeometry args={[wallThickness, wallHeight, roomSize]} />
-        <meshLambertMaterial color="#F5F5DC" />
+        <meshLambertMaterial color={wallColor} />
       </mesh>
-      
+
       {/* Desk and Computer Setup */}
       <Desk />
     </group>
