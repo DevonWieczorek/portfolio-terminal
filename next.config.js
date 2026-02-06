2776dfb9c207cd7b6af77a085aaae014c48b3402
Devon Wieczorek <devon.wieczorek93@gmail.com>
2026-02-06 16:41:41 -0500

    feat: adds ifdef-loader via webpack plugin
    removes the need for babel all together
    preserves `// #if DEBUG` syntax

--- next.config.js
+++ next.config.js
@@ -1,9 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-        output: 'standalone',
-        eslint: {
-                ignoreDuringBuilds: false,
-        },
+    output: "standalone",
+    eslint: {
+        ignoreDuringBuilds: false,
+    },
+    webpack: (config, { isServer }) => {
+        config.module.rules.push({
+            test: /\.(js|ts|tsx)$/,
+            exclude: /node_modules/,
+            enforce: "pre",
+            use: [
+                {
+                    loader: "ifdef-loader",
+                    options: {
+                        DEBUG: process.env.NEXT_PUBLIC_DEBUG === "true",
+                        VERSION: 1,
+                        "ifdef-verbose": false,
+                        "ifdef-triple-slash": false,
+                    },
+                },
+            ],
+        });
+        return config;
+    },
 };
 
 module.exports = nextConfig;
