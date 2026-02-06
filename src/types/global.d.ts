4a26b1c55a0ee840fd231802315ce02c9e81db35
Devon Wieczorek <devon.wieczorek93@gmail.com>
2026-02-06 14:06:25 -0500

    style: fixes indentation

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,15 +2,13 @@ import { Vector3 } from "three";
 export {};
 
 declare global {
+    type DeepPartial<T> = {
+        [P in keyof T]?: T[P] extends object
+            ? T[P] extends Function
+                ? T[P]
+                : DeepPartial<T[P]>
+            : T[P];
+    };
 
-  type DeepPartial<T> = {
-    [P in keyof T]?: T[P] extends object
-      ? T[P] extends Function
-        ? T[P]
-        : DeepPartial<T[P]>
-      : T[P];
-  };
-
-  type PositionArray = [number, number, number] | Vector3;
-
+    type PositionArray = [number, number, number] | Vector3;
 }
