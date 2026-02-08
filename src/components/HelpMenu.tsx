ecfa23e913378e69a022b839ebdf4b06d5a02802
Devon Wieczorek <devon.wieczorek93@gmail.com>
2026-02-08 18:04:22 -0500

    fix(HelpMenu): fixes render logic for desktop version message

--- src/components/HelpMenu.tsx
+++ src/components/HelpMenu.tsx
@@ -12,19 +12,18 @@ const HelpMenu = () => {
             <p>fun-fact: displays a random &quot;fun fact&quot; about Devon</p>
             <p>clear: clear the console</p>
             <p>help: displays this menu</p>
-            {!isMobile && (
-                <>
-                    <p>
-                        <br />
-                        <br />
-                        View this page on desktop for the full, 3D experience.
-                    </p>
-                    <p>
-                        <br />
-                        <br />
-                        Press ESC to exit computer.
-                    </p>
-                </>
+            {!isMobile ? (
+                <p>
+                    <br />
+                    <br />
+                    Press ESC to exit computer.
+                </p>
+            ) : (
+                <p>
+                    <br />
+                    <br />
+                    View this page on desktop for the full, 3D experience.
+                </p>
             )}
         </div>
     );
