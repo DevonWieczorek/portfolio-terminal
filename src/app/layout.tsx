67fc91dd8379401aaf4419d0ca33c607ae8aa90a
Devon Wieczorek <devon.wieczorek93@gmail.com>
2026-03-04 16:57:43 -0500

    feat: adds vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import "@/styles/reset.scss";
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <body suppressHydrationWarning>
                 {children}
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
